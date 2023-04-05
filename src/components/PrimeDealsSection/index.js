40965cfefe660d83b4626470072a48324a0b54b7
pravinDevabhatini <praveend070@gmail.com>
2023-04-05 05:01:04 +0000

    pratice

---
+++ src/components/PrimeDealsSection/index.js
@@ -0,0 +1,105 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+
+import ProductCard from '../ProductCard'
+import './index.css'
+
+const apisConstants = {
+  initial: 'INITIAl',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class PrimeDealsSection extends Component {
+  state = {
+    primeDeals: [],
+    apiStatus: apisConstants.Initial,
+  }
+
+  componentDidMount() {
+    this.getPrimeDeals()
+  }
+
+  getPrimeDeals = async () => {
+    this.setState({
+      apiStatus: apisConstants.inProgress,
+    })
+
+    const jwtToken = Cookies.get('jwt_token')
+
+    const apiUrl = 'https://apis.ccbp.in/prime-deals'
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(apiUrl, options)
+    if (response.ok === true) {
+      const fetchedData = await response.json()
+      const updatedData = fetchedData.prime_deals.map(product => ({
+        title: product.title,
+        brand: product.brand,
+        price: product.price,
+        id: product.id,
+        imageUrl: product.image_url,
+        rating: product.rating,
+      }))
+      this.setState({
+        primeDeals: updatedData,
+        apiStatus: apisConstants.success,
+      })
+    } else if (response.status === 401) {
+      this.setState({
+        apiStatus: apisConstants.failure,
+      })
+    }
+  }
+
+  renderPrimeDealsList = () => {
+    const {primeDeals} = this.state
+    return (
+      <div className="products-list-container">
+        <h1 className="primeDeals-list-heading">Exclusive Prime Deals</h1>
+        <ul className="products-list">
+          {primeDeals.map(product => (
+            <ProductCard productData={product} key={product.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  renderPrimeDealsFailureView = () => (
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/exclusive-deals-banner-img.png"
+      alt="Register Prime"
+      className="register-prime-image"
+    />
+  )
+
+  renderLoadingView = () => (
+    <div className="products-loader-container">
+      <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
+    </div>
+  )
+
+  render() {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apisConstants.success:
+        return this.renderPrimeDealsList()
+      case apisConstants.failure:
+        return this.renderPrimeDealsFailureView()
+      case apisConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+}
+
+export default PrimeDealsSection
